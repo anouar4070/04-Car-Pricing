99061e365e1c101204d5dc7ad715331a078bdcb8
anouar garbaa <g.anouar@yahoo.com>
2025-09-03 17:40:02 +0200

    using SerializeInterceptor with  serDto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -6,25 +6,26 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto';
 
-interface ClassConstructor {
-  new (...args: any[]): {};
-}
+// interface ClassConstructor {
+//   new (...args: any[]): {};
+// }
 
-export function Serialize(dto: ClassConstructor) {
-  return UseInterceptors(new SerializeInterceptor(dto));
-}
+// export function Serialize(dto: ClassConstructor) {
+//   return UseInterceptors(new SerializeInterceptor(dto));
+// }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  //constructor(private dto: any) {}
 
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // * Run something before a request is handled
+    //  Run something before a request is handled
     return handler.handle().pipe(
       map((data: any) => {
-        // * Run something before the response is sent out
-        return plainToClass(this.dto, data, {
+        //  Run something before the response is sent out
+        return plainToInstance(UserDto, data, {
           excludeExtraneousValues: true,
         });
       }),
