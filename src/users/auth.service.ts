9f7a169f5a96971b6a053d634a54640536d11995
anouar garbaa <g.anouar@yahoo.com>
2025-09-04 15:39:00 +0200

    Sign up functionality: Seeing if email is in use

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   //make copy of UsersService
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    //1/ See if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+//     //2/ Hash the users password
+//     // Generate a salt
+//     const salt = randomBytes(8).toString('hex');
+
+//     // Hash the salt and the password together
+//     const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+//     // Join the hashed result and the salt together
+//     const result = salt + '.' + hash.toString('hex');
+
+//     // Create a new user and save it
+// / return the user
+  }
+
+  signin() {}
 }
