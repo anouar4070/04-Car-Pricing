c7988d3abdf17dfe2dd144a912a4f2371f96c7d7
anouar garbaa <g.anouar@yahoo.com>
2025-09-08 20:19:25 +0200

    Refactoring to use intelligent Mocks

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -52,8 +52,7 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await service.signup('asdf@asdf.com', 'asdf');
 
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow();
   });
