8bb18e13083c0a04e50fedcf253393c8a3f22419
anouar garbaa <g.anouar@yahoo.com>
2025-09-15 18:56:59 +0200

    setting up a global Pipe from inside  App Module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { setupApp } from './setup-app';
+import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
 
 //const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupApp(app);
+  app.use(
+    cookieSession({
+      keys: ['asdfasfd'],
+    }),
+  );
+  app.useGlobalPipes(
+    ,
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
